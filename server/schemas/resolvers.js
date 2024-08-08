44d2205d9920a0439a6a8384a04f8eb549e4559c
amandeep <samandeepkaue34@gmail.com>
2024-08-08 19:08:26 -0400

    updated resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -74,7 +74,6 @@ const resolvers = {
         );
       }
       throw new AuthenticationError('Not authenticated');
-    },
   },
     addThought: async (parent, { thoughtText }, context) => {
       if (context.user) {
