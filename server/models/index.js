adf00b7b717e716a1478cd74771c3603715f84fe
amandeep <samandeepkaue34@gmail.com>
2024-08-09 13:21:12 -0400

    imported category and log models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,7 @@
 const User = require('./User');
 const Thought = require('./Thought');
+const ExerciseCategory = require('/ExerciseCategory');
+const ExerciseLog = require('./ExerciseLog');
 
-module.exports = { User, Thought };
+
+module.exports = { User, Thought, ExerciseCategory, ExerciseLog };
