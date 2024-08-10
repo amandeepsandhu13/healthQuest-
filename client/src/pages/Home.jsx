e19a2bdb530f1cf3abe52d40501c043074cc7ed1
amandeep <samandeepkaue34@gmail.com>
2024-08-10 13:07:54 -0400

    Log link added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,17 @@
 import { useQuery } from "@apollo/client";
-
-// import ThoughtList from "../components/ThoughtList";
-// import ThoughtForm from "../components/ThoughtForm";
-
-// import { QUERY_THOUGHTS } from "../utils/queries";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    // const { loading, data } = useQuery(QUERY_THOUGHTS);
-    // const thoughts = data?.thoughts || [];
-
+   
     return (
         <main>
             <div className="flex-row justify-center">
-                {/* <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ThoughtForm />
-        </div> */}
-                {/* <div className="col-12 col-md-8 mb-3">
-                    {loading ? (
-                        <div>Loading...</div>
-                    ) : (
-                        <ThoughtList
-                            thoughts={thoughts}
-                            title="Some Feed for Thought(s)..."
-                        />
-                    )}
-                </div> */}
+            <div className="card-body">
+            <p>Welcome to HealthQuest! Select an option below to manage your exercise logs.</p>
+            <Link to="/log-exercise" className="btn btn-primary m-2">Log New Exercise</Link>
+            {/* <Link to="/add-exercise-category" className="btn btn-primary m-2">Add Categories</Link> */}
+            
+          </div>
             </div>
         </main>
     );
