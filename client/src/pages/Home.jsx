1e96995cf5289a74715abaa59a6eef7735bb62e8
amandeep <samandeepkaue34@gmail.com>
2024-08-10 18:07:41 -0400

    view log link added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,9 @@ const Home = () => {
             <div className="card-body">
             <p>Welcome to HealthQuest! Select an option below to manage your exercise logs.</p>
             <Link to="/log-exercise" className="btn btn-primary m-2">Log New Exercise</Link>
+            <Link className="btn btn-primary m-2" to="/me">
+                View Activities Log
+              </Link>
             {/* <Link to="/add-exercise-category" className="btn btn-primary m-2">Add Categories</Link> */}
             
           </div>
