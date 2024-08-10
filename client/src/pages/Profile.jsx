d0542a99e46e49e0a83ad4ece49bf53f81c4f8a7
amandeep <samandeepkaue34@gmail.com>
2024-08-10 16:11:30 -0400

    code formatting

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,81 +1,93 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
 
 const Profile = () => {
-    const { username: userParam } = useParams();
+  const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
 
-  if (userParam && Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  if (
+    userParam &&
+    Auth.loggedIn() &&
+    Auth.getProfile().data.username === userParam
+  ) {
     return <Navigate to="/me" />;
   }
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    if (!user?.username) {
-        return (
-            <h4>
-                You need to be logged in to see this. Use the navigation links
-                above to sign up or log in!
-            </h4>
-        );
-    }
-
+  if (!user?.username) {
     return (
-        <div>
-            <div className="flex-row justify-center mb-3">
-                <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-                    Viewing {userParam ? `${user.username}'s` : "your"} profile.
-                </h2>
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
 
-  {/* Exercise Log List */}
-  <div className="col-12 col-md-10 mb-5">
-        <h3>Exercise Logs:</h3>
-        {user.exerciseLogs.map((log) => (
-          <div key={log._id} className="card mb-3">
-            <div className="card-body">
-              <h4 className="card-title">Category: {log.category}</h4>
-              <p>Duration: {log.duration} minutes</p>
-              <p>Date: {new Date(parseInt(log.date)).toLocaleDateString()}</p>
-              
-              {/* Dynamically Render category-specific data */}
-              {log.categorySpecificData && (
-                <div>
-                  {Object.keys(log.categorySpecificData).map((key) => {
-                      if (key === '__typename') return null; // Skip the __typename field
+  return (
+    <div>
+      <div className="flex-row justify-center mb-3">
+        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
+        </h2>
 
-                    const categoryData = log.categorySpecificData[key];
-                    if (categoryData && Object.keys(categoryData).length > 0) {
-                      return (
-                        <div key={key}>
-                          <h5>{key.charAt(0).toUpperCase() + key.slice(1)} Details:</h5>
-                          {Object.entries(categoryData).map(([field, value]) => (
-                            value && (
-                              <p key={field}>
-                                {field.charAt(0).toUpperCase() + field.slice(1)}: {value}
-                              </p>
-                            )
-                          ))}
-                        </div>
-                      );
-                    }
-                    return null;
-                  })}
-                </div>
-              )}
+        {/* Exercise Log List */}
+        <div className="col-12 col-md-10 mb-5">
+          <h3>Exercise Logs:</h3>
+          {user.exerciseLogs.map((log) => (
+            <div key={log._id} className="card mb-3">
+              <div className="card-body">
+                <h4 className="card-title">Category: {log.category}</h4>
+                <p>Duration: {log.duration} minutes</p>
+                <p>Date: {new Date(parseInt(log.date)).toLocaleDateString()}</p>
+
+                {/* Dynamically Render category-specific data */}
+                {log.categorySpecificData && (
+                  <div>
+                    {Object.keys(log.categorySpecificData).map((key) => {
+                      if (key === "__typename") return null; // Skip the __typename field
+                      const categoryData = log.categorySpecificData[key];
+                      if (
+                        categoryData &&
+                        Object.keys(categoryData).length > 0
+                      ) {
+                        return (
+                          <div key={key}>
+                            <h5>
+                              {key.charAt(0).toUpperCase() + key.slice(1)}{" "}
+                              Details:
+                            </h5>
+                            {Object.entries(categoryData).map(
+                              ([field, value]) =>
+                                value && (
+                                  <p key={field}>
+                                    {field.charAt(0).toUpperCase() +
+                                      field.slice(1)}
+                                    : {value}
+                                  </p>
+                                )
+                            )}
+                          </div>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </div>
   );
