e1a963c91f2b6bda2d0f71dd77752ff2f20c9278
amandeep <samandeepkaue34@gmail.com>
2024-08-11 11:23:38 -0400

    home updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,28 +40,9 @@ const Home = () => {
                 View Activities Log
               </Link>
             {/* <Link to="/add-exercise-category" className="btn btn-primary m-2">Add Categories</Link> */}
-            
+            </div>
           </div>
-      {/* <div className="flex-row justify-center"> */}
-      {/* <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ThoughtForm />
-        </div> */}
-      {/* <div className="col-12 col-md-8 mb-3">
-                    {loading ? (
-                        <div>Loading...</div>
-                    ) : (
-                        <ThoughtList
-                            thoughts={thoughts}
-                            title="Some Feed for Thought(s)..."
-                        />
-                    )}
-                </div> */}
-      {/* </div> */}
-
-      {/* Banner Section */}
+    
       <section className="banner-home">
         <div className="banner-home-content">
           <h1>YOUR PERSONAL FITNESS JUST GOT BETTER</h1>
