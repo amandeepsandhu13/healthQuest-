d19cbf355c6349e985b2d5f87b06c857d19c23fe
Natalie Clinton <natalie.clinton@hotmail.com>
2024-08-13 19:53:59 -0400

    more bug fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom/dist";
-import 'primereact/resources/primereact.min.css'; 
-import 'primeicons/primeicons.css';
+// import 'primereact/resources/themes/saga-blue/theme.css'
+// import 'primereact/resources/primereact.min.css'; 
+// import 'primeicons/primeicons.css';
 import "./index.css";
 
 import App from "./App.jsx";
