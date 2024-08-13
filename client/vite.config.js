5ae23e0a404ec6ca25df82529a7237ddadda2819
amandeep <samandeepkaue34@gmail.com>
2024-08-13 19:52:09 -0400

    external lib

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +11,13 @@ export default defineConfig({
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['primereact'], // Replace 'module-name' with the actual module name causing the issue
+    },
+  },
+});
