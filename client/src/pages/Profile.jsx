ebe4be1d2e7b2539a73f8f4cec36fd557ebfac4f
amandeep <samandeepkaue34@gmail.com>
2024-08-13 16:29:48 -0400

    dropdow issue  fixed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -94,7 +94,7 @@ const Profile = () => {
                                         : {value}
                                       </p>
                                     )
-                                )}
+                                )} 
                               </div>
                             );
                           }
